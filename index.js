be6d70918f251992d96acb3def58ac8a6cbc7814
Lance Carlson <lancecarlson@gmail.com>
2017-10-10 17:22:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-'use strict'
+'use strict';
+const mapObj = require('map-obj');
+const pascalCase = require('pascal-case');
+const QuickLru = require('quick-lru');
 
-var mapObj = require('map-obj')
-var pascalCase = require('pascal-case')
+const has = (arr, key) => arr.some(x => typeof x === 'string' ? x === key : x.test(key));
+const cache = new QuickLru({maxSize: 100000});
 
-module.exports = function (obj) {
-  return mapObj(obj, function (key, val) {
-    return [pascalCase(key), val]
-  })
-}
+module.exports = (input, opts) => {
+	opts = Object.assign({
+		deep: false
+	}, opts);
+
+	const exclude = opts.exclude;
+
+	return mapObj(input, (key, val) => {
+		if (!(exclude && has(exclude, key))) {
+			if (cache.has(key)) {
+				key = cache.get(key);
+			} else {
+				const ret = pascalCase(key);
+
+				if (key.length < 100) { // Prevent abuse
+					cache.set(key, ret);
+				}
+
+				key = ret;
+			}
+		}
+
+		return [key, val];
+	}, {deep: opts.deep});
+};
