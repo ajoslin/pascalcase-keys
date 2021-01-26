a84ae191e34e869504a9fbf2d991f0aed8ab2f08
Luis Rodríguez <lrodriguezp.18@gmail.com>
2021-01-25 21:55:21 -0400

    Updating index.js to add behavior with arrays

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ var mapObj = require('map-obj')
 var pascalCase = require('pascal-case')
 
 module.exports = function (obj) {
-  return mapObj(obj, function (key, val) {
-    return [pascalCase(key), val]
-  })
+	if (Array.isArray(obj)) {
+			return obj.map(o => mapObj(o, mapObjCallBack));
+	} else {
+			return mapObj(obj, mapObjCallBack);
+	}
+}
+
+const mapObjCallBack = (key, val) => {
+	return [pascalCase(key), val]
 }
